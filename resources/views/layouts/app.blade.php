5cb108516d1bb8427a2a0a61babb977e75fc6e51
Sazzad Hossain Nirjhor <sazzad3029@gmail.com>
2021-02-22 16:37:42 +0600

    game-2 drag option

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,13 +15,16 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/game2.css') }}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <script src="https://code.jquery.com/jquery-3.5.1.js"
-            integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
-            crossorigin="anonymous"></script>
+        integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
     @livewireStyles
 
     {{-- Livewire chart --}}
@@ -37,23 +41,25 @@
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
 
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js" integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
+        integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU=" crossorigin="anonymous"></script>
 
 
 </head>
+
 <body class="font-sans antialiased">
 
-{{-- {{asset('css/app.css')}} --}}
-<div class="min-h-screen bg-gray-100">
-    @livewire('navigation-dropdown')
+    {{-- {{asset('css/app.css')}} --}}
+    <div class="min-h-screen bg-gray-100">
+        @livewire('navigation-dropdown')
 
 
-            <!-- Page Heading -->
-    {{-- <header class="bg-white shadow">
+        <!-- Page Heading -->
+        {{-- <header class="bg-white shadow">
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             {{ $header }}
 
-        </div>
+    </div>
     </header> --}}
 
     <!-- Page Content -->
@@ -66,27 +72,32 @@
         {{-- @yield('nextUrl') --}}
         <div class="next_previous_container">
             @if(\Illuminate\Support\Facades\View::hasSection('nextUrl'))
-                <a href="@yield('nextUrl')" id="arrow_next" class="arrow"><span>&#9658;&#9658;</span></a>
+            <a href="@yield('nextUrl')" id="arrow_next" class="arrow"><span>&#9658;&#9658;</span></a>
             @endif
             @if(\Illuminate\Support\Facades\View::hasSection('previousUrl'))
-                <a href="@yield('previousUrl')" id="arrow_previous" class="arrow"><span>&#9668;&#9668;</span></a>
+            <a href="@yield('previousUrl')" id="arrow_previous" class="arrow"><span>&#9668;&#9668;</span></a>
             @endif
         </div>
 
-        
-        
+
+
     </main>
 
-    
 
-</div>
 
-@stack('modals')
+    </div>
 
-@livewireScripts
+    @stack('modals')
 
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
-<script src="{{asset('js/chart.js')}}"></script>
+    @livewireScripts
+
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+        integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+        integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+    </script>
+    <script src="{{asset('js/chart.js')}}"></script>
 </body>
+
 </html>
