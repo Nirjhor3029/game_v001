b96358262ea8ef4af37cbcf02b0b753900f7c4f4
Sazzad Hossain Nirjhor <sazzad3029@gmail.com>
2021-02-23 15:39:25 +0600

    create game2 layout & 3 new page

---
+++ resources/views/game_views/gm2/marketing_strategy.blade.php
@@ -0,0 +1,20 @@
+@extends('game_views.gm2.layout.app')
+
+@section('content')
+
+<div class="gm2">
+
+    <div class="market_strategy">
+        <img src="{{asset('assets/img/marketing_strategy.JPG')}}" class="image" alt="">
+        <h2>Strategic Groups</h2>
+
+        <img src="{{asset('assets/img/marketing_strategy_table_1.JPG')}}" class="image" alt="">
+        <h2>Market Scenario Table</h2>
+        <img src="{{asset('assets/img/marketing_strategy_table_2.JPG')}}" class="image" alt="">
+        <h2>Market Scenario Table</h2>
+    </div>
+
+</div>
+
+
+@endsection
