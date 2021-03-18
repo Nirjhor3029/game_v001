a1d0099ee587384d9f80874b4fbdffb374408fe4
Sazzad Hossain Nirjhor <sazzad3029@gmail.com>
2021-03-18 10:32:03 +0600

    outlets column added

---
+++ database/migrations/2021_03_18_042618_add_outlets_column_market_costs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddOutletsColumnMarketCostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('market_costs', function (Blueprint $table) {
+            $table->integer("number_of_outlets")->default(1);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('market_costs', function (Blueprint $table) {
+            //
+        });
+    }
+}
